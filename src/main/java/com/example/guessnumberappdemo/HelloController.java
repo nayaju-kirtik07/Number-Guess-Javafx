ffb10524dc08f2d09f7452820ad1ea94ba8ab93f
Kritik Nayaju <kirtiknayaju@gmail.com>
2023-08-16 10:25:53 +0545

    timer and dialogues are added

--- src/main/java/com/example/guessnumberappdemo/HelloController.java
+++ src/main/java/com/example/guessnumberappdemo/HelloController.java
@@ -1,26 +1,43 @@
 package com.example.guessnumberappdemo;
 
+import javafx.application.Platform;
+import javafx.beans.binding.Bindings;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 import static jdk.tools.jlink.internal.plugins.PluginsResourceBundle.getMessage;
 
 public class HelloController {
-    int systemGeneratedNumber;
-    int guessCount = 0;
+    private final IntegerProperty countDownTime = new SimpleIntegerProperty();
+    int systemGeneratedNumber, guessCount = 0, randomNumber;
     @FXML
-    private Label hintLabel;
+    private Label hintLabel, countGuessLabel, timerLabel;
     @FXML
     private TextField numberInputField;
     @FXML
-    private Label countGuessLabel;
+    private Button guessButton, onStartClick;
+    private int COUNT_DOWN = 30;
+    private boolean stopCountdown = false;
 
 
-    public HelloController() {
-        this.systemGeneratedNumber = this.generateRandomNumber();
+    @FXML
+    private void initialize() {
+        timerLabel.textProperty().bind(Bindings.format("Time remaining: %d", countDownTime));
+        disableButton(true);
     }
+
+    public void disableButton(boolean flag) {
+        numberInputField.setDisable(flag);
+        guessButton.setDisable(flag);
+        countGuessLabel.setVisible(!flag);
+    }
+
 
     public void Counter() {
         guessCount++;
@@ -28,8 +45,55 @@ public class HelloController {
     }
 
     public int generateRandomNumber() {
-        return (int) (Math.random() * 100) + 1;
+        randomNumber = (int) (Math.random() * 100) + 1;
+        System.out.println("random number :"+randomNumber);
+        return randomNumber;
     }
+
+    public void displayGameOverDialog() {
+        Alert alert = new Alert(Alert.AlertType.WARNING);
+        alert.setHeaderText("Game Over!!");
+        alert.setContentText("Correct number : %d\nTotal attempt : %d".formatted(systemGeneratedNumber, guessCount));
+        alert.showAndWait();
+    }
+
+    public void displaySuccessDialog() {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setHeaderText("Congratulation!!");
+        alert.setContentText("Correct number : %d\nTotal attempt : %d".formatted(systemGeneratedNumber, guessCount));
+        alert.showAndWait();
+    }
+
+
+    public void startCountDown(int inputValue) {
+        if (inputValue > 0) {
+            stopCountdown = false;
+            countDownTime.set(inputValue);
+            Thread countdownThread = new Thread(() -> {
+                while (countDownTime.get() > 0 && !stopCountdown) {
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    int newValue = countDownTime.get() - 1;
+                    Platform.runLater(() -> countDownTime.set(newValue));
+                }
+
+                if (countDownTime.get() <= 0) {
+                    Platform.runLater(() -> {
+                        countDownTime.set(0);
+                        disableButton(true);
+                        displayGameOverDialog();
+                    });
+                }
+            });
+            countdownThread.start();
+
+
+        }
+    }
+
 
     public void OnGuessButtonClick(ActionEvent actionEvent) {
 
@@ -55,6 +119,7 @@ public class HelloController {
                 Counter();
             } else {
                 hintLabel.setText("Congratulation you have guessed correct number!!");
+                displaySuccessDialog();
             }
 
 
@@ -65,8 +130,15 @@ public class HelloController {
         }
     }
 
-    public void OnRestartClick(ActionEvent actionEvent) {
+    public void onStartClick(ActionEvent actionEvent) {
+        disableButton(false);
+        systemGeneratedNumber = this.generateRandomNumber();
         guessCount = 0;
+        onStartClick.setText("Restart");
         countGuessLabel.setText("Count : " + guessCount);
+        numberInputField.setText("");
+        startCountDown(COUNT_DOWN);
+
     }
+
 }
